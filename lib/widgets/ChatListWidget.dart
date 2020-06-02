e852528fc24de541f5d4dc54e69dad2cb01cc914
sujan karki <sujankarki269@gmail.com>
2020-06-02 10:28:59 +0545

    commit

--- lib/widgets/ChatListWidget.dart
+++ lib/widgets/ChatListWidget.dart
@@ -7,7 +7,7 @@ class ChatListWidget extends StatelessWidget{
   final ScrollController listScrollController = new ScrollController();
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    
     return Flexible(
         child: ListView.builder(
           padding: EdgeInsets.all(10.0),
